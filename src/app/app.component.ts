3d312e4bdb49141705a457f886222d10190bba35
Vitaliy Budkin <bvfromru@gmail.com>
2023-05-27 23:15:44 +0400

    fix: fix styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Airways Angular App';
+  title = 'Angular Airways App';
 
   isMainPage: boolean;
 
